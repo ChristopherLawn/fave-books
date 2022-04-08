670dd707799a554d8008714b9ef983f1caa2f359
CLAW <christopher.lawn@yahoo.com>
2022-04-08 14:44:02 -0700

    updated SearchBooks.js & SavedBooks.js, replaced API calls with queries & mutations, removed excess unused code

--- server/server.js
+++ server/server.js
@@ -37,16 +37,18 @@ const startServer = async () => {
 
   // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`🌍 Now listening on localhost:${PORT}`)
-      // log where we can go to test our GQL API
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    });
-  });
 
 };
 
 // Initialize the Apollo server
 startServer();
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
+    // log where we can go to test our GQL API
+
+  });
+});
